7c4c0a0ba78e95f59066be7b21078a504ec90b19
Popniten <popniten.eriksson@gmail.com>
2018-11-30 19:21:07 +0100

    Adding mutations, actions etc.

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -2,23 +2,48 @@
   <div class="stats">
     <h1>A different component</h1>
     <p>There are currently {{ countLinks }} links</p>
+
+    <button v-on:click="removeAllLinks">Remove all links</button>
+    <p>{{ msg }}</p>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations, mapActions } from 'vuex'
 
 export default {
   name: 'Stats',
+  data () {
+    return {
+      msg: ''
+    }
+  },
   computed: {
     ...mapGetters([
       'countLinks'
     ])
+  },
+  methods: {
+    ...mapMutations(['REMOVE_ALL_LINKS']),
+    ...mapActions(['removeAll']),
+    removeAllLinks () {
+      this.removeAll().then(() => {
+        this.msg = 'They have all been removed'
+      })
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+button {
+  padding: 10px;
+  margin-top: 30px;
+  width: 100%;
+  background: none;
+  border: 1px solid lightgray;
+  outline: 0;
+  cursor: pointer;
+}
 </style>
